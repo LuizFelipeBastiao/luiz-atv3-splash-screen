b82f140286ce1f3682a85c8e42431853853614e8
Luiz Felipe Bastião <lbastiao@alunos.utfpr.edu.br>
2025-05-07 12:30:44 -0300

    iniciado a implementação de um widget do onboarding

--- luiz_atv3_splash_screen/lib/screens/onboarding/onboarding.dart
+++ luiz_atv3_splash_screen/lib/screens/onboarding/onboarding.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+
 import 'package:flutter/material.dart';
 import 'package:onboarding/onboarding.dart';
 import 'onboardingTela1.dart';
@@ -14,18 +16,40 @@ class TelaOnboarding extends StatefulWidget {
 // Tela de Onboarding
 // Esta tela é responsável por exibir o processo de onboarding do aplicativo
 class _TelaOnboardingState extends State<TelaOnboarding> {
+  int _paginaAtual = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: (Onboarding(
-        swipeableBody: [
-          // Lista de widgets que podem ser arrastados horizontalmente
-          // Cada widget representa uma tela de onboarding
-          TelaOnboarding1(),
-          TelaOnboarding2(),
-          TelaOnboarding3(),
+      body: Stack(
+        children: [
+          (Onboarding(
+            swipeableBody: [
+              // Lista de widgets que podem ser arrastados horizontalmente
+              // Cada widget representa uma tela de onboarding
+              TelaOnboarding1(),
+              TelaOnboarding2(),
+              TelaOnboarding3(),
+            ],
+          )),
+          Column(children: [
+          const SizedBox(height: 100,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(3, (index) {
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                width: _paginaAtual == index ? 24:16,
+                height: _paginaAtual == index ? 24:16,
+                decoration: BoxDecoration(
+                  color: _paginaAtual == index ? Colors.blue : Colors.grey,
+                  shape : BoxShape.circle
+                ),
+              );
+            }),
+          ),
         ],
-      )),
-    );
+        )],
+    ));
   }
 }
